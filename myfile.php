b24c654d0d4f992ed358a6d74d253352d44c07e0
expertechcom <133237802+expertechcom@users.noreply.github.com>
2023-05-25 14:36:32 +0100

    Create myfile.php

---
+++ myfile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Data</title>
+</head>
+<body>
+
+<?php
+// Student Data
+$name = "UDE LAWRENCE OKO";
+$registrationNumber = "2021/HND/33734/CS";
+$stateOfOrigin = "Ebonyi";
+$projectTopic = " Design and Implementation of Road Accident Alert Management System: A study of Peace Mass Transit Limited, Afikpo Branch, Ebonyi State";
+
+// Output the data 
+echo "<h1>Student Data</h1>";
+echo "<p><strong>Name:</strong> " . $name . "</p>";
+echo "<p><strong>Registration Number:</strong> " . $registrationNumber . "</p>";
+echo "<p><strong>State of Origin:</strong> " . $stateOfOrigin . "</p>";
+echo "<p><strong>Project Topic:</strong> " . $projectTopic . "</p>";
+?>
+
+</body>
+</html>
